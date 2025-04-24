b1862a27e1b1aaa7641b3399df2e9bc8388da887
Shakir-007 <56887751+Shakir-007@users.noreply.github.com>
2025-04-24 10:10:39 +0530

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -3,13 +3,13 @@
 
 terraform {
 
-  cloud {
-    organization = "organization-name"
+  # cloud {
+  #   organization = "HCP-007"
 
-    workspaces {
-      name = "learn-terraform"
-    }
-  }
+  #   workspaces {
+  #     name = "learn-terraform"
+  #   }
+  # }
 
   required_providers {
     aws = {
